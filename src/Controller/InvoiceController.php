0010f828e3f00b762127c6d4a39bc83ba390fb57
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-03-10 19:32:23 +0100

    Only allow invoice id to have digits

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,14 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\AppUser;
-use App\Entity\Invoice;
 use App\Service\InvoiceService;
-use App\Utils\InvoiceUtils;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InvoiceController extends AbstractController
@@ -47,6 +45,12 @@ class InvoiceController extends AbstractController
     #[Route('invoices/{id}', name: 'invoice_details', methods: ['GET'])]
     public function getInvoiceById(EntityManagerInterface $entityManager, string $id): JsonResponse
     {
+        if (!ctype_digit($id)) {
+            return $this->json([
+                'error' => 'Invalid id. Only digits are allowed.'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
         $response = $this->invoiceService->getInvoiceById($entityManager, self::USER_ID, intval($id));
         return $this->json([
             'data' => $response
